400f88c4250e628cf837167d342e83fe6e673d2c
David Esperalta <davidesperalta@gmail.com>
2025-06-29 11:35:42 +0200

    Minor fixes
    Some minor fixes

--- www/Humm/Sites/Shared/Classes/SharedView.php
+++ www/Humm/Sites/Shared/Classes/SharedView.php
@@ -6,10 +6,10 @@ use
   \Humm\System\Classes\HummView,
   \Humm\System\Classes\HtmlTemplate;
 
-class SharedView extends HummView
-{
-  public function __construct(HtmlTemplate $template)
-  {
+final class SharedView extends HummView {
+
+  public function __construct (HtmlTemplate $template) {
+
     parent::__construct($template);
   }
 }
