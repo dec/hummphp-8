287bf4e2194302c69e7890b1bb8b5af7d42f70b6
David Esperalta <davidesperalta@gmail.com>
2026-03-25 12:22:28 +0100

    Minor changes
    Minor changes

--- www/Humm/System/Classes/Encrypter.php
+++ www/Humm/System/Classes/Encrypter.php
@@ -29,9 +29,6 @@ final class Encrypter extends Unclonable {
   /**
    * Encrypt the specified text string.
    *
-   * This method encrypt the specified text string using
-   * the also provided "secret key" and "secret IV".
-   *
    * @static
    * @param string $clear_text String to be encrypted
    * @param string $secret_key The secret key to be used
@@ -60,22 +57,19 @@ final class Encrypter extends Unclonable {
   /**
    * Decrypt the specified previously encrypted string.
    *
-   * This method decrypt the specified previously encrypted text
-   * string using the also provided "secret key" and "secret IV".
-   *
    * @static
-   * @param string $clear_text Base64 codified string to be decrypted
+   * @param string $encrypted_text Base64 codified string to be decrypted
    * @param string $secret_key The secret key used when encrypt
    * @param string $cipher The cipher algorithm to be used
    * @return string|null The decrypted text or null if an error occur
    */
-  public static function decrypt (string $clear_text, string $secret_key, string $cipher = 'AES-256-CBC') : string|null {
+  public static function decrypt (string $encrypted_text, string $secret_key, string $cipher = 'AES-256-CBC') : string|null {
 
     if (!\in_array(\strtolower($cipher), \openssl_get_cipher_methods())) {
       throw new \Exception('Non available cipher.');
     }
 
-    $decoded_clear_text = base64_decode($clear_text);
+    $decoded_clear_text = base64_decode($encrypted_text);
 
     $base64_secret_key = \base64_encode($secret_key);
 
