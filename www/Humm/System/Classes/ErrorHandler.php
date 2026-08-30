32e0f961e615c77edef7cebe24a6b9874d4dc270
David Esperalta <davidesperalta@gmail.com>
2026-08-30 10:19:03 +0200

    Fix a bug related with error handler
    Fix a bug related with error handler

--- www/Humm/System/Classes/ErrorHandler.php
+++ www/Humm/System/Classes/ErrorHandler.php
@@ -67,13 +67,12 @@ final class ErrorHandler extends Unclonable {
 
       self::$errors = [];
 
-      if (!\HUMM_HANDLE_ERRORS) {
-        return;
-      }
-
       \set_error_handler([__CLASS__, self::ERROR_HANDLER]);
-      \set_exception_handler([__CLASS__, self::EXCEPTION_HANDLER]);
-      \register_shutdown_function([__CLASS__, self::SHUTDOWN_HANDLER]);
+
+      if (\HUMM_HANDLE_ERRORS) {
+        \set_exception_handler([__CLASS__, self::EXCEPTION_HANDLER]);
+        \register_shutdown_function([__CLASS__, self::SHUTDOWN_HANDLER]);
+      }
     }
   }
 
